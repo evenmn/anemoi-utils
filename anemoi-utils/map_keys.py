faf48390b9a714815bf281680ba15a512d7bb7ad
evenmn <evenmn@fys.uio.no>
2024-12-20 15:21:33 +0200

    Implemented verification against point observations

--- anemoi-utils/map_keys.py
+++ anemoi-utils/map_keys.py
@@ -28,6 +28,7 @@ map_keys = {
     'air_temperature_2m': {
         'standard': 't2m',
         'era5': ['2t'],
+        'frost': 'air_temperature',
         'units': 'C',
         'transform': kelvin2celsius,
         'thresholds': [-30, -20, -10, 0, 10, 20, 25, 30, 35, 40, 45, 50],
@@ -35,7 +36,8 @@ map_keys = {
     },
     'wind_speed_10m': {
         'standard': 'ws10m',
-        'era5': ['10u', '10v'], 
+        'era5': ['10u', '10v'],
+        'frost': 'wind_speed',
         'units': 'm/s', 
         'transform': wind_magnitude,
         'thresholds': [10.8, 13.9, 17.2, 20.8, 24.5, 28.5, 32.6],
@@ -44,6 +46,7 @@ map_keys = {
     'precipitation_amount_acc6h': {
         'standard': 'precip6h',
         'era5': ['tp'],
+        'frost': 'sum(precipitation_amount PT6H)',
         'units': 'mm',
         'transform': precip_accu6,
         'thresholds': [0.5, 1, 5, 10, 20, 30, 40, 50],
@@ -52,6 +55,7 @@ map_keys = {
     'air_pressure_at_sea_level': {
         'standard': 'mslp',
         'era5': ['msl'],
+        'frost': 'air_pressure_at_sea_level',
         'units': 'hPa',
         'transform': mslp_,
         'thresholds': [970, 980, 990, 1000, 1010, 1020, 1030],
